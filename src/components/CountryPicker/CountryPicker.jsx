ee80bc6e3add878e77eeb00b17ac7499e5056c4a
FelipeCosta <felipesaev@gmail.com>
2020-04-19 19:56:23 -0300

    add features in components

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+import styles from './CountryPicker.module.css';
+
+import {fetchCountries} from '../../api';
 
 const CountryPicker = () => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+  useEffect(()=> {
+    const fetchAPI = async () => {
+      setFetchedCountries( await fetchCountries());
+    }
+    fetchAPI();
+  }, [setFetchedCountries]);
+  
   return (
     <div>
-      <h1>
-        CountryPicker
-      </h1>
+      <FormControl className={styles.container}>
+        <NativeSelect>
+          <option value="Global">Global</option>
+          {fetchedCountries.map((country, indice) => 
+            <option key={indice} value={country}>{country}</option>
+          )}
+        </NativeSelect>
+      </FormControl>
     </div>
   )
 }
